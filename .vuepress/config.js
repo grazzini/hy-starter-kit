6aa18aa315df1488e4af36eda4f688b26e9afbb8
Steve Grazzini <steve.grazzini@grantstreet.com>
2018-08-22 07:05:43 -0400

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -19,15 +19,33 @@ module.exports = {
         children: [
           ['/unit-1/', 'Materials'],
           '/unit-1/w1',
-//        '/unit-1/w2',
+          '/unit-1/w2',
 //        '/unit-1/w3',
+//        '/unit-1/w4',
+//        '/unit-1/w5',
         ]
       },
 //     {
 //        title: 'Unit 2'
+//        children: [
+//          ['/unit-2/', 'Materials'],
+//          '/unit-2/w6',
+//          '/unit-2/w7',
+//          '/unit-2/w8',
+//          '/unit-2/w9',
+//          '/unit-2/w10',
+//        ],
 //      },
 //      {
 //        title: 'Unit 3'
+//        children: [
+//          ['/unit-2/', 'Materials'],
+//          '/unit-3/w11',
+//          '/unit-3/w12',
+//          '/unit-3/w13',
+//          '/unit-3/w14',
+//          '/unit-3/w15',
+//        ],
 //      },
     ]
   }
